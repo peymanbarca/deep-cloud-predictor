d545c48847856cb81b9355043c6fe1ba844a8aad
vacek <p.yazdanian74@gmail.com>
2019-04-05 15:06:28 +0430

    bilstm-convnet for Nasa completed

---
+++ NASA-HTTP/prediction/LSTM-Convnet/1min/Read_Data.py
@@ -0,0 +1,16 @@
+import psycopg2
+import numpy as np
+
+hostname = 'localhost'
+username = 'postgres'
+password = 'inter2010'
+database = 'load_cloud'
+
+conn = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
+
+def read_data():
+    cur1=conn.cursor()
+    cur1.execute('select ts,num_of_req from nasa_http_1min order by ts offset 400')
+    data=np.array(cur1.fetchall())
+    print('data read from DB!')
+    return data
