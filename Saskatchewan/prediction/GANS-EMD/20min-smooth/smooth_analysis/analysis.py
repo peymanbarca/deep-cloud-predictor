665798bb2cdab94fe82c6075c21ddeac7d252dc2
vacek <p.yazdanian74@gmail.com>
2019-05-10 21:33:38 +0430

    gans sascatchwen hybrid added 30 min

---
+++ Saskatchewan/prediction/GANS-EMD/20min-smooth/smooth_analysis/analysis.py
@@ -0,0 +1,33 @@
+import psycopg2
+import numpy as np
+from matplotlib import pyplot as plt
+
+hostname = 'localhost'
+username = 'postgres'
+password = 'inter2010'
+database = 'load_cloud'
+
+conn = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
+cur1=conn.cursor()
+
+imf_num=3
+
+
+cur1.execute('select ts,num_of_req from saskatchewan_http_emd_20min where imf_index=%s and num_req_pred is null'
+             ' order by ts limit 100',([int(imf_num)]))
+data=np.array(cur1.fetchall())
+ts=data[:,0]
+req=data[:,1]
+
+cur1.execute('select ts,num_of_req from saskatchewan_http_emd_20min_copy where imf_index=%s and num_req_pred is null'
+             ' order by ts limit 100',([int(imf_num)]))
+data_smooth=np.array(cur1.fetchall())
+req_smooth=data_smooth[:,1]
+
+
+
+plt.plot(ts, req, label='real data')
+plt.plot(ts, req_smooth,'-.', 'green', label='smooth data ')
+plt.legend()
+plt.show()
+
