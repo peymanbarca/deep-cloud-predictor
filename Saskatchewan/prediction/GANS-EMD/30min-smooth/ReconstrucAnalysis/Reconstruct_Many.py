4200d3d9e14c0010e6eeb388d2967f2d401f85e1
vacek <p.yazdanian74@gmail.com>
2019-05-10 20:55:06 +0430

    gans sascatchwen hybrid added

--- Saskatchewan/prediction/GANS-EMD/30min-smooth/ReconstrucAnalysis/Reconstruct_Many.py
+++ Saskatchewan/prediction/GANS-EMD/30min-smooth/ReconstrucAnalysis/Reconstruct_Many.py
@@ -15,11 +15,12 @@ conn = psycopg2.connect(host=hostname, user=username, password=password, dbname=
 cur0=conn.cursor()
 
 
-
 start_imf=1
+imfs1=[1,2,3]
+imfs2=[4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
 
 def mean_absolute_percentage_error(y_true, y_pred):
-
+    #y_true, y_pred = np.abs(y_true), np.abs(y_pred)
     ape = []
     for k in range(len(y_true)):
         #if abs(y_true[k])!=0 and k not in z1 and k not in z2:
@@ -29,7 +30,7 @@ def mean_absolute_percentage_error(y_true, y_pred):
     plt.xlabel('MAPE')
     plt.ylabel('frequency')
     plt.grid()
-    plt.savefig('/home/vacek/Cloud/cloud-predictor/Saskatchewan/prediction/GANS-EMD/30min/resutls'
+    plt.savefig('/home/vacek/Cloud/cloud-predictor/Saskatchewan/prediction/GANS-EMD/30min-smooth/resutls'
                 '/MAPE_from_imf_' + str(start_imf) + '.png', dpi=600)
     plt.pause(3)
     plt.close()
@@ -41,7 +42,7 @@ def mean_absolute_percentage_error(y_true, y_pred):
     return np.mean(np.array(ape)) * 100
 
 def mean_percentage_error(y_true, y_pred):
-
+    #y_true, y_pred = np.abs(y_true), np.abs(y_pred)
     ape = []
     for k in range(len(y_true)):
         #if abs(y_true[k])!=0  and k not in z1 and k not in z2:
@@ -53,7 +54,7 @@ def mean_percentage_error(y_true, y_pred):
     return np.mean(np.array(ape)) * 100
 
 def median_absolute_percentage_error(y_true, y_pred):
-
+    #y_true, y_pred = np.abs(y_true), np.abs(y_pred)
     ape = []
     for k in range(len(y_true)):
         if abs(y_pred[k]) > 1e-3 and abs(y_true[k]) > 1e-3:
@@ -65,7 +66,7 @@ def median_absolute_percentage_error(y_true, y_pred):
     return np.median(np.array(ape)) * 100
 
 def mean_percentage_r_error(y_true, y_pred):
-
+    #y_true, y_pred = np.abs(y_true), np.abs(y_pred)
     ape = []
     for k in range(len(y_true)):
         if abs(y_pred[k]) > 1e-3 and abs(y_true[k]) > 1e-3:
@@ -87,13 +88,14 @@ main_test_req_pred=[]
 
 
 
-for i in range(start_imf,20):
+for i in imfs1:
+
     print(i,' ...')
     emd_imf=i
 
 
 
-    cur0.execute('select ts,num_of_req,num_req_pred_svr from saskatchewan_http_emd_30min where imf_index=%s'
+    cur0.execute('select ts,num_of_req,num_req_pred_gan from saskatchewan_http_emd_30min_copy where imf_index=%s'
                  ' and num_req_pred is null and num_req_pred_gan is not null'
                  ' order by ts',([int(emd_imf)]))
     data=np.array(cur0.fetchall())
@@ -107,11 +109,11 @@ for i in range(start_imf,20):
     main_test_req_pred.append(list(num_req_pred))
 
     print('length of test set: ',len(test_ts),len(num_req),len(num_req_pred))
-    cur0.execute('select count(1) from saskatchewan_http_emd_30min where imf_index=1 and '
+    cur0.execute('select count(1) from saskatchewan_http_emd_30min_copy where imf_index=1 and '
                  'num_req_pred is null and num_req_pred_gan is  null')
     total=cur0.fetchall()
     total=np.array(total)[0][0]
-    cur0.execute('select ts,num_of_req from saskatchewan_http_emd_30min where imf_index=%s and '
+    cur0.execute('select ts,num_of_req from saskatchewan_http_emd_30min_copy where imf_index=%s and '
                  'num_req_pred is null and num_req_pred_gan is null '
                  ' order by ts limit %s ', (int(emd_imf),int(total)-1))
     data = np.array(cur0.fetchall())
@@ -120,6 +122,41 @@ for i in range(start_imf,20):
     train_ts = ts_train
     print('length of train set: ', len(ts_train), len(num_req_train))
     main_train_req.append(list(num_req_train))
+
+for j in imfs2:
+
+    print('----------------------',j,'---------- from LSTM-EMD Method')
+    cur0.execute(
+        'select ts,num_of_req,num_req_pred from saskatchewan_http_emd_30min_copy where imf_index=%s and num_req_pred is not null'
+        ' order by ts', ([int(j)]))
+    data = np.array(cur0.fetchall())
+
+    ts = data[:, 0]
+    num_req = data[:, 1]
+    num_req_pred = data[:, 2]
+
+    num_req=np.delete(num_req,[0,1])
+    num_req_pred=np.delete(num_req_pred,[0,1])
+    ts = np.delete(ts, [0,1])
+
+    test_ts = ts
+
+    main_test_req.append(list(num_req))
+    main_test_req_pred.append(list(num_req_pred))
+    print('length of test set: ', len(test_ts), len(num_req), len(num_req_pred))
+    cur0.execute('select count(1) from saskatchewan_http_emd_30min_copy where imf_index=1 and num_req_pred is null')
+    total = cur0.fetchall()
+    total = np.array(total)[0][0]
+    cur0.execute('select ts,num_of_req from saskatchewan_http_emd_30min_copy where imf_index=%s and num_req_pred is  null '
+                 ' order by ts limit %s', (int(j), int(total - len(test_ts))-1 ))
+    data = np.array(cur0.fetchall())
+    ts_train = data[:, 0]
+    num_req_train = data[:, 1]
+    train_ts = ts_train
+    print('length of train set: ', len(ts_train), len(num_req_train))
+    main_train_req.append(list(num_req_train))
+
+
 
 main_train_req__=np.zeros(len(train_ts))
 for k in main_train_req:
@@ -151,15 +188,15 @@ plt.plot(test_ts, main_test_req_, color='blue',alpha=0.5,
 plt.plot(test_ts,main_test_req_pred_,'-.',color='green',
          label='Prediction Req')
 ax = fig.add_subplot(212)
-plt.plot(ts,main_test_req_,'-',color='blue',label='Real Req',alpha=0.4)
-plt.plot(ts,main_test_req_pred_,'-',color='green',alpha=0.9,
+plt.plot(ts,main_test_req_,'-.',color='blue',label='Real Req',alpha=0.9)
+plt.plot(ts,main_test_req_pred_,'-',color='green',alpha=0.4,
          label=('Prediction Req, MAPE = %.4f%% ,  RMSE=%.4f , MPE=%.4f%% ,\n  MEAPE=%.4f%%, RMSRE=%4f '% (MAPE,rms,MPE,MEAPE,RMSRE)))
 plt.xlabel('TS for test part')
 plt.ylabel('Num of Req')
 plt.legend()
 plt.grid()
-plt.savefig('/home/vacek/Cloud/cloud-predictor/Saskatchewan/prediction/GANS-EMD/30min/resutls'
-            '/main_reconstruct_from_imf_'+str(start_imf) + '.png', dpi=600)
+plt.savefig('/home/vacek/Cloud/cloud-predictor/Saskatchewan/prediction/GANS-EMD/30min-smooth/resutls'
+           '/main_reconstruct_from_imf_'+str(start_imf) + '.png', dpi=600)
 plt.pause(7)
 plt.close()
 
---
+++ Saskatchewan/prediction/GANS-EMD/30min-smooth/smoother.py
@@ -0,0 +1,73 @@
+import psycopg2
+import numpy as np
+
+hostname = 'localhost'
+username = 'postgres'
+password = 'inter2010'
+database = 'load_cloud'
+
+conn = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
+cur1=conn.cursor()
+
+def read_data(imf_num):
+
+    cur1.execute('select ts,num_of_req from saskatchewan_http_emd_30min where imf_index=%s and num_req_pred is null'
+                 ' order by ts',([int(imf_num)]))
+    data=np.array(cur1.fetchall())
+    print('data read from DB!')
+    return data
+
+
+def smooth(x,size=5):
+    from pandas import Series
+    from matplotlib import pyplot
+    import pandas as pd
+
+    data=x
+    x=pd.DataFrame(data=data    # values
+              )
+    rolling = x.rolling(window=size)
+    rolling_mean = rolling.mean()
+    #print(rolling_mean.head(10))
+    # plot original and transformed dataset
+    x.plot()
+    rolling_mean.plot(color='red')
+    rolling_mean=rolling_mean.dropna()
+    pyplot.pause(3)
+    pyplot.close()
+    return np.array(rolling_mean)
+
+
+
+from numpy import *
+from matplotlib import pyplot as plt
+
+
+def smooth_demo(imf_index):
+    data = read_data(imf_index)
+    ts = data[:, 0]
+    num_req = data[:, 1]
+
+    size=10
+    num_req_smooth = smooth(num_req,size=size)
+    num_req_smooth= np.concatenate([np.zeros(shape=(size-1,1)), num_req_smooth])
+
+    print('----------')
+    print(len(ts),len(num_req_smooth),len(num_req))
+    plt.subplot(211)
+    plt.plot(ts,num_req,label='real data')
+    plt.subplot(212)
+    plt.plot(ts,num_req_smooth,'green',label='smooth data ')
+    plt.legend()
+    plt.show()
+    #
+    for k in range(len(ts)):
+        print(k)
+        cur1.execute('update saskatchewan_http_emd_30min_copy set num_of_req=%s where imf_index=%s'
+                    ' and ts=%s', \
+                    (int(num_req_smooth[k]),int(imf_index), int(ts[k])))
+        conn.commit()
+
+
+if __name__ == '__main__':
+    smooth_demo(3)
