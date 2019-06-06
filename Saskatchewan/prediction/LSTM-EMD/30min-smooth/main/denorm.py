69e675bfd3afe010775871ba8e70597003e9e39f
vacek <p.yazdanian74@gmail.com>
2019-06-06 21:06:38 +0430

    many runs done for smooth versions other methods sasck

---
+++ Saskatchewan/prediction/LSTM-EMD/30min-smooth/main/denorm.py
@@ -0,0 +1,14 @@
+
+def denorm_v1(min1,max1,min2,max2,ts_normalize):
+    ts=ts_normalize * (max1-min1)
+    return min1,max1,min2,max2,ts
+
+def denorm_v2(ts_normalize,MaxAbsScalerObj):
+    from sklearn import preprocessing
+
+    ts_revert = MaxAbsScalerObj.inverse_transform(ts_normalize.reshape(-1, 1))
+    print('-----------------------', len(ts_revert), len(ts_normalize))
+    min2 = min(ts_revert)
+    max2 = max(ts_revert)
+    return  min2, max2, ts_revert
+
