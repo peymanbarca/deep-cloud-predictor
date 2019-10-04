da26139b2fff054ddfa3f65359652639c8745f55
vacek <p.yazdanian74@gmail.com>
2019-10-04 13:50:29 +0330

    lstm only runned for nasa fix bug

--- NASA-HTTP/prediction/1step/LSTM-Convnet/5min/Read_Data.py
+++ NASA-HTTP/prediction/1step/LSTM-Convnet/5min/Read_Data.py
@@ -10,7 +10,7 @@ conn = psycopg2.connect(host=hostname, user=username, password=password, dbname=
 
 def read_data():
     cur1=conn.cursor()
-    cur1.execute('select ts,num_of_req from nasa_http_5min order by ts offset 400')
+    cur1.execute('select ts,num_of_req from nasa_http_5min where num_req_pred is null order by ts offset 400')
     data=np.array(cur1.fetchall())
     print('data read from DB!')
     return data
